e9181390a39ab8a90cf2cd289be3834143563b18
Eriel Cabrera <erielcabrera1234@gmail.com>
2024-11-09 17:18:18 -0500

    added library

--- .pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
+++ .pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -1,4 +1,4 @@
 .pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.d
+++ .pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.d
@@ -1,12 +1,12 @@
 .pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.o: \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
- C:/Users/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /home/eriel/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h
